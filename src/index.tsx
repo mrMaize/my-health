c82ab85adb2fb6168ca475533a3e91f5d54de950
Artem Kukuruzza <asekukuruza@sberbank.ru>
2024-04-24 23:00:34 +0300

    firebase

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
 import 'normalize.css';
+import { initializeApp } from 'firebase/app';
+import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
+
 import { App } from './app';
+
+// Import the functions you need from the SDKs you need
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyC1MpPx-hBHiidUgoq09Rg5CQcK75gaOCU',
+  authDomain: 'my-health-59d4d.firebaseapp.com',
+  projectId: 'my-health-59d4d',
+  storageBucket: 'my-health-59d4d.appspot.com',
+  messagingSenderId: '555746872791',
+  appId: '1:555746872791:web:147c98adcb14a2d83ecf92',
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
     <App />
--- src/pages/login/LoginPage.tsx
+++ src/pages/login/LoginPage.tsx
@@ -1,22 +1,44 @@
 import { FC, useState } from 'react';
+import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
 
 import { Panel, Title, Input, Button } from '../../shared/components';
 import { CenteredPage } from '../../layouts';
 import { EButtonVariant } from '../../shared/components/Button';
+import { sendRequest } from '../../shared/api/xhr/xhr';
+import { ERequestTypes } from '../../shared/api/xhr/requestConfig';
+
+const auth = getAuth();
 
 const LoginPage: FC = () => {
   const [login, setLogin] = useState<string>('');
   const [password, setPassword] = useState<string>('');
 
   const handleLogin = () => {
-    console.log('attempt to login');
+    console.log('attempt to login...');
+    sendRequest(ERequestTypes.GET, {
+      url: 'http://localhost:5000/v1/user/login',
+    });
+  };
+
+  const logIn = () => {
+    createUserWithEmailAndPassword(auth, login, password)
+      .then((userCredential) => {
+        // Signed up
+        const user = userCredential.user;
+        // ...
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // ..
+      });
   };
 
   return (
     <CenteredPage>
       <Panel>
         <Title>Авторизация</Title>
-        <form onSubmit={handleLogin}>
+        <form>
           <Input value={login} onChange={setLogin} label={'Логин'} />
           <Input
             label={'Пароль'}
@@ -24,10 +46,10 @@ const LoginPage: FC = () => {
             value={password}
             onChange={setPassword}
           />
-          <Button type="submit" variant={EButtonVariant.FILLED}>
-            Войти
-          </Button>
         </form>
+        <Button type="submit" variant={EButtonVariant.FILLED} onClick={logIn}>
+          Войти
+        </Button>
       </Panel>
     </CenteredPage>
   );
