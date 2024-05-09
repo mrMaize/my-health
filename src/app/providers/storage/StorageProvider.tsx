bbc686dc745606c9f5cd835b60c0288a2518ca06
Artem Kukuruzza <asekukuruza@sberbank.ru>
2024-05-09 19:02:49 +0300

    подготовка состояния на redux-toolkit

---
+++ src/app/providers/storage/StorageProvider.tsx
@@ -0,0 +1,10 @@
+import { FC, PropsWithChildren } from 'react';
+import { Provider } from 'react-redux';
+
+import { store } from '../../store/root';
+
+const StorageProvider: FC<PropsWithChildren> = ({ children }) => {
+  return <Provider store={store}>{children}</Provider>;
+};
+
+export { StorageProvider };
---
+++ src/shared/storage/reducers/userReducer.ts
@@ -0,0 +1,31 @@
+import { PayloadAction, createSlice } from '@reduxjs/toolkit';
+
+import { IUser } from '../../types/user';
+
+type IUserState = {
+  user: null | IUser;
+  isAuth: boolean;
+  token: null | string;
+};
+
+const initialState: IUserState = {
+  user: null,
+  isAuth: false,
+  token: null,
+};
+
+const userReducer = createSlice({
+  name: 'user',
+  initialState,
+  reducers: {
+    setUserAuth: (state, action: PayloadAction<IUserState['isAuth']>) => {
+      state.isAuth = action.payload;
+    },
+    setUser: (state, action: PayloadAction<IUserState['user']>) => {
+      state.user = action.payload;
+    },
+  },
+});
+
+export const { setUserAuth, setUser } = userReducer.actions;
+export default userReducer.reducer;
